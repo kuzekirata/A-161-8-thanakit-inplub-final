18be34f5bedbcc6728bbdf815e529575e1c9b163
kuzekirata <Thanakit.inpl@bumail.net>
2025-12-02 10:43:52 +0700

    Update PowerUpBase.cs

--- Assets/Scripts/PowerUpBase.cs
+++ Assets/Scripts/PowerUpBase.cs
@@ -2,16 +2,8 @@ using UnityEngine;
 
 public class PowerUpBase : MonoBehaviour
 {
-    public bool IsEffectActive()
-    {
-
-    }
 
     public override ApplyEffect(Player player)
-    {
-    }
-
-    public abstract ApplyEffect(Player player);
     {
     }
 }
